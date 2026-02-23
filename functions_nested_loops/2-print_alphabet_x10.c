44b65b3c561dc7b6a5884998dd4c99ba3c2e9d8f
leen <leenalmj7d@gmail.com>
2026-02-23 22:15:43 +0000

    Add 2-print_alphabet_x10.c script

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -5,12 +5,12 @@
  */
 void print_alphabet_x10(void)
 {
-    int i, c;
+	int i, c;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (c = 'a'; c <= 'z'; c++)
-            _putchar(c);
-        _putchar('\n');
-    }
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');
+	}
 }
