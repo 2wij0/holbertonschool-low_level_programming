f9a8372dd8dfe8d53ee5762036b377f60cc02de7
leen <leenalmj7d@gmail.com>
2026-02-23 23:14:27 +0000

    Add 7-print_last_digit.c script

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,18 +2,20 @@
 
 /**
  * print_last_digit - prints the last digit of a number
- * @n: the number to process
+ * @n: the number to check
  *
- * Return: value of the last digit
+ * Return: the value of the last digit
  */
 int print_last_digit(int n)
 {
-    int last;
+	int last;
 
-    last = n % 10;       /* get last digit */
-    if (last < 0)
-        last = -last;    /* make positive if negative */
+	last = n % 10;
 
-    _putchar('0' + last); /* print as character */
-    return (last);
+	if (last < 0)
+		last = -last;
+
+	_putchar(last + '0');
+
+	return (last);
 }
