15a31ff332e89b1151c0acf0583146ecac4ac4fe
leen <leenalmj7d@gmail.com>
2026-03-01 19:50:51 +0000

    add 103-infinite_add.c script

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -1,6 +1,27 @@
 #include "main.h"
 
 /**
+ * rev_string - reverses a string
+ * @n: the string to reverse
+ */
+void rev_string(char *n)
+{
+	int i = 0, j = 0;
+	char temp;
+
+	while (n[j] != '\0')
+		j++;
+	j--;
+
+	for (i = 0; i < j; i++, j--)
+	{
+		temp = n[i];
+		n[i] = n[j];
+		n[j] = temp;
+	}
+}
+
+/**
  * infinite_add - adds two numbers
  * @n1: first number string
  * @n2: second number string
@@ -14,20 +35,16 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	int i = 0, j = 0, k = 0, sum, carry = 0;
 	int len1 = 0, len2 = 0;
 
-	/* Find lengths of input strings */
 	while (n1[len1])
 		len1++;
 	while (n2[len2])
 		len2++;
 
-	/* Check if result + null terminator can fit in buffer */
-	if (len1 + 2 > size_r || len2 + 2 > size_r)
+	if (len1 >= size_r || len2 >= size_r)
 		return (0);
 
 	i = len1 - 1;
 	j = len2 - 1;
-
-	/* Add digits from right to left */
 	while (i >= 0 || j >= 0 || carry)
 	{
 		sum = carry;
@@ -35,22 +52,12 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			sum += n1[i--] - '0';
 		if (j >= 0)
 			sum += n2[j--] - '0';
-
 		if (k >= size_r - 1)
 			return (0);
-
 		r[k++] = (sum % 10) + '0';
 		carry = sum / 10;
 	}
 	r[k] = '\0';
-
-	/* Reverse the result string in buffer r */
-	for (i = 0, j = k - 1; i < j; i++, j--)
-	{
-		char temp = r[i];
-		r[i] = r[j];
-		r[j] = temp;
-	}
-
+	rev_string(r);
 	return (r);
 }
