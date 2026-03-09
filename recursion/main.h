a7f91e085134be2c16aecc1d49392c8951883e85
leen <leenalmj7d@gmail.com>
2026-03-09 17:32:14 +0000

    add main.h script

---
+++ recursion/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Prototype for the custom _putchar function */
+int _putchar(char c);
+
+/* Prototype for your recursion function */
+void _puts_recursion(char *s);
+
+#endif /* MAIN_H */
