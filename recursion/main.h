9769c645ba7e777ab74bb4d5a03a8decc35048e1
leen <leenalmj7d@gmail.com>
2026-03-09 17:51:22 +0000

    add main.h script

--- recursion/main.h
+++ recursion/main.h
@@ -1,10 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Prototype for the custom _putchar function */
+
+
+
+
+
+int factorial(int n);
 int _putchar(char c);
 int _strlen_recursion(char *s);
-/* Prototype for your recursion function */
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 #endif /* MAIN_H */
