5d64116d0762c339fcbcbb47c64de64f192edc85
leen <leenalmj7d@gmail.com>
2026-03-10 18:13:37 +0000

    add script

--- argc_argv/3-args.c
+++ argc_argv/3-args.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - prints all arguments it receives, starting from the first one
+ * main - multiplies two numbers
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: Always 0 (Success)
+ * Return: 0 (Success), 1 (Error)
  */
 int main(int argc, char *argv[])
 {
-	int i;
+	int num1, num2, result;
 
-	for (i = 1; i < argc; i++)
+	if (argc != 3)
 	{
-		printf("%s\n", argv[i]);
+		printf("Error\n");
+		return (1);
 	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
 
 	return (0);
 }
