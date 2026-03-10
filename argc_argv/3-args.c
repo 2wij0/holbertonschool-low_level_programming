42d9eb922da60a6e4b6a690a6a5d9774fd589c5b
leen <leenalmj7d@gmail.com>
2026-03-10 18:06:45 +0000

    add 3-args.c script

---
+++ argc_argv/3-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives, starting from the first one
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
