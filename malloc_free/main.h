ebc67ddc5a9cabf0b9cc42e2664f3979763cc46c
leen <leenalmj7d@gmail.com>
2026-03-30 14:12:37 +0000

    add script

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -4,6 +4,7 @@
 /* Prototype for task 0 */
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+char *argstostr(int ac, char **av);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 char *argstostr(int ac, char **av);
