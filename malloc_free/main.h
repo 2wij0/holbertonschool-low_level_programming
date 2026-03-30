1013853831c53895be8679a984a902356b72a1b7
leen <leenalmj7d@gmail.com>
2026-03-30 14:15:45 +0000

    add script

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 /* Prototype for task 0 */
+char **strtow(char *str);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *argstostr(int ac, char **av);
