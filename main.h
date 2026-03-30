0da9ea5af46594fe7b569ca99b591175a4fc4785
leen <leenalmj7d@gmail.com>
2026-03-30 13:31:59 +0000

    add main.h

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Prototype for task 0 */
+char *create_array(unsigned int size, char c);
+
+/* You will add more prototypes here as you progress through the project */
+
+#endif /* MAIN_H */
