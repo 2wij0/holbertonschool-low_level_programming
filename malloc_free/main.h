14992aa5cbd00c47adddd0892e6a0569f46a819a
leen <leenalmj7d@gmail.com>
2026-03-30 13:42:31 +0000

    add script

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -3,7 +3,7 @@
 
 /* Prototype for task 0 */
 char *create_array(unsigned int size, char c);
-
+char *_strdup(char *str);
 /* You will add more prototypes here as you progress through the project */
 
 #endif /* MAIN_H */
