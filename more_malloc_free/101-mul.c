de5290851143436315bf253c92edf2194e1149c7
leen <leenalmj7d@gmail.com>
2026-03-31 12:58:00 +0000

    add script

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -3,10 +3,9 @@
 #include <stdio.h>
 
 /**
- * is_digit - Checks if a string contains only digits.
- * @s: The string to be evaluated.
- *
- * Return: 1 if all digits, 0 otherwise.
+ * is_digit - checks if a string contains only digits
+ * @s: string to check
+ * Return: 1 if all digits, 0 otherwise
  */
 int is_digit(char *s)
 {
@@ -22,10 +21,9 @@ int is_digit(char *s)
 }
 
 /**
- * _strlen - Returns the length of a string.
- * @s: The string to evaluate.
- *
- * Return: The length of the string.
+ * _strlen - returns length of a string
+ * @s: string
+ * Return: length
  */
 int _strlen(char *s)
 {
@@ -37,61 +35,51 @@ int _strlen(char *s)
 }
 
 /**
- * errors - Handles errors for main.
- */
-void errors(void)
-{
-	printf("Error\n");
-	exit(98);
-}
-
-/**
- * main - Multiplies two positive numbers.
- * @argc: Number of arguments.
- * @argv: Array of arguments.
- *
- * Return: Always 0.
+ * main - multiplies two positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, 98 on error
  */
 int main(int argc, char *argv[])
 {
-	char *s1, *s2;
-	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+	char *s1 = argv[1], *s2 = argv[2];
+	int len1, len2, len, i, j, carry, digit1, digit2, *res, found = 0;
 
-	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
-		errors();
+	{
+		printf("Error\n");
+		exit(98);
+	}
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2;
-	result = malloc(sizeof(int) * len);
-	if (!result)
+	res = calloc(len, sizeof(int));
+	if (!res)
 		return (1);
-	for (i = 0; i <= len; i++)
-		result[i] = 0;
-	for (len1 = len1 - 1; len1 >= 0; len1--)
+	for (i = len1 - 1; i >= 0; i--)
 	{
-		digit1 = s1[len1] - '0';
+		digit1 = s1[i] - '0';
 		carry = 0;
-		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		for (j = len2 - 1; j >= 0; j--)
 		{
-			digit2 = s2[len2] - '0';
-			carry += result[len1 + len2 + 1] + (digit1 * digit2);
-			result[len1 + len2 + 1] = carry % 10;
+			digit2 = s2[j] - '0';
+			carry += res[i + j + 1] + (digit1 * digit2);
+			res[i + j + 1] = carry % 10;
 			carry /= 10;
 		}
 		if (carry > 0)
-			result[len1 + len2 + 1] += carry;
+			res[i + j + 1] += carry;
 	}
-	for (i = 0; i < len - 1; i++)
+	for (i = 0; i < len; i++)
 	{
-		if (result[i])
-			a = 1;
-		if (a)
-			_putchar(result[i] + '0');
+		if (res[i])
+			found = 1;
+		if (found)
+			_putchar(res[i] + '0');
 	}
-	_putchar(result[len - 1] + '0');
+	if (!found)
+		_putchar('0');
 	_putchar('\n');
-	free(result);
+	free(res);
 	return (0);
 }
-
