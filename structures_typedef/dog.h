438dbc81f77b116dfab3117512090151013bfe58
leen <leenalmj7d@gmail.com>
2026-04-04 10:31:32 +0000

    add script

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A new type describing a dog
+ * @name: First member, name of the dog (char *)
+ * @age: Second member, age of the dog (float)
+ * @owner: Third member, owner of the dog (char *)
+ *
+ * Description: This structure stores basic identification and
+ * age information for a dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
